0c14956b83609471c3a618bb685a838cb29cd546
Damian Kreft <damiankreft87@gmail.com>
2021-11-28 18:54:43 +0100

    Move lecture 5 to a separate directory. Add lecture 6 project.

---
+++ restjpademo/src/main/java/pl/pja/edu/mp/springboot/restjpademo/RestjpademoApplication.java
@@ -0,0 +1,13 @@
+package pl.pja.edu.mp.springboot.restjpademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestjpademoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestjpademoApplication.class, args);
+	}
+
+}
