c35d403f2a67ef8d30fea7c6a7d78b88c2f1e4b7
Damian Kreft <damiankreft87@gmail.com>
2021-11-28 19:40:00 +0100

    Add /api/person create and getAll methods.

--- restjpademo/src/main/java/pl/pja/edu/mp/springboot/restjpademo/web/PersonController.java
+++ restjpademo/src/main/java/pl/pja/edu/mp/springboot/restjpademo/web/PersonController.java
@@ -1,10 +1,33 @@
 package pl.pja.edu.mp.springboot.restjpademo.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import pl.pja.edu.mp.springboot.restjpademo.domain.Person;
+import pl.pja.edu.mp.springboot.restjpademo.service.PersonManager;
+
+import java.util.List;
 
 @RestController
 public class PersonController {
+
+    private final PersonManager personManager;
+
+    public PersonController(PersonManager personManager) {
+        this.personManager = personManager;
+    }
+
+    @PostMapping("api/person")
+    Person addPerson(@RequestBody Person person) {
+        return personManager.addPerson(person);
+    }
+
+    @GetMapping("api/person")
+    List<Person> getAll() {
+        return personManager.getAllPersons();
+    }
+
     @GetMapping("api/hello")
     String hello() {
         return "Hello!";
