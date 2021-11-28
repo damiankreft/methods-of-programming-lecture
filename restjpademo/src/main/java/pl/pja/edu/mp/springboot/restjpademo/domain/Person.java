8d09d5f3d007c2cc20b6e5322b4f89eb421799d7
Damian Kreft <damiankreft87@gmail.com>
2021-11-28 21:14:35 +0100

    Add application setup on start.

--- restjpademo/src/main/java/pl/pja/edu/mp/springboot/restjpademo/domain/Person.java
+++ restjpademo/src/main/java/pl/pja/edu/mp/springboot/restjpademo/domain/Person.java
@@ -1,5 +1,7 @@
 package pl.pja.edu.mp.springboot.restjpademo.domain;
 
+import java.util.Objects;
+
 public class Person {
 
     private String id;
@@ -8,6 +10,11 @@ public class Person {
 
     public Person(String id, String firstName, int yob) {
         this.id = id;
+        this.firstName = firstName;
+        this.yob = yob;
+    }
+
+    public Person(String firstName, int yob) {
         this.firstName = firstName;
         this.yob = yob;
     }
@@ -36,5 +43,27 @@ public class Person {
 
     public void setYob(int yob) {
         this.yob = yob;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id='" + id + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", yob=" + yob +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return yob == person.yob && Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, yob);
     }
 }
